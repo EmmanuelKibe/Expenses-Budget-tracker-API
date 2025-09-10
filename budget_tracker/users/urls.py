7cd3a62b17bc37ea3907d01869f4d869664fc5c9
Emmanuel <emmanuelkibe148@gmail.com>
2025-09-10 14:10:54 +0300

    Configured the /me url pattern

--- budget_tracker/users/urls.py
+++ budget_tracker/users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView
+from .views import RegisterView, UserDetailView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", TokenObtainPairView.as_view(), name="login"),
     path("refresh/", TokenRefreshView.as_view(), name="refresh"),
+    path("me/", UserDetailView.as_view(), name="user_detail"),
 ]
 
 
