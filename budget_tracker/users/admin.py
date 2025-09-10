e9f3bf63877a554d1ccf18b78bae33536aae39cb
Emmanuel <emmanuelkibe148@gmail.com>
2025-09-10 14:13:24 +0300

    Ran migrations on the users model

--- budget_tracker/users/admin.py
+++ budget_tracker/users/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ("id", "username", "email", "is_staff", "is_active")
+    search_fields = ("username", "email")
+    ordering = ("id",)
+
+admin.site.register(CustomUser, CustomUserAdmin)
