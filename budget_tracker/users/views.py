74b624d702d35b8077c6d8e891729f27f302b44e
Emmanuel <emmanuelkibe148@gmail.com>
2025-09-10 14:10:00 +0300

    Added a /me endpoint for the user to fetch their own details

--- budget_tracker/users/views.py
+++ budget_tracker/users/views.py
@@ -1,7 +1,16 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import CustomUser
 from .serializers import UserSerializer
 
 class RegisterView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
+
+
+class UserDetailView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
