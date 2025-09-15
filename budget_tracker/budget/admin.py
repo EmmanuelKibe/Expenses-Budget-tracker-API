cd38cdfd89e331731b638307164da6e467fca779
Emmanuel <emmanuelkibe148@gmail.com>
2025-09-15 08:46:03 +0300

    register the budget model

--- budget_tracker/budget/admin.py
+++ budget_tracker/budget/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from .models import Budget
+
+@admin.register(Budget)
+class BudgetAdmin(admin.ModelAdmin):    
+    list_display = ("id", "user", "name", "amount", "start_date", "end_date", "created_at", "updated_at")
+    search_fields = ("name", "user__username", "user__email")
+    list_filter = ("start_date", "end_date", "created_at", "updated_at")
+    ordering = ("-created_at",)
