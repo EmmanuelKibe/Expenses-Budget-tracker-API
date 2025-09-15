fb2b073864a8ceb649a1d671d8a0d7fc86f8f875
Emmanuel <emmanuelkibe148@gmail.com>
2025-09-15 09:20:18 +0300

    Viewset for CRUD operations

--- budget_tracker/budget/views.py
+++ budget_tracker/budget/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets, permissions
+from .models import Budget
+from .serializers import BudgetSerializer
+
+class BudgetViewSet(viewsets.ModelViewSet):
+    serializer_class = BudgetSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Budget.objects.filter(user=self.request.user)
