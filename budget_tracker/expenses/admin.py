ca0be1c12bb8a86e78f26c9f2bc68afa48d34ffc
Emmanuel <emmanuelkibe148@gmail.com>
2025-09-17 15:56:54 +0300

    Add ExpenseAdmin class to manage expense models in the admin interface

--- budget_tracker/expenses/admin.py
+++ budget_tracker/expenses/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "amount", "date", "user", "budget")
+    search_fields = ("name", "user__username", "budget__name")
+    list_filter = ("date", "budget")
+    ordering = ("-date",)
+
+    
