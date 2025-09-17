f2986a6a3a42cb12adeef9c536e1524460392f22
Emmanuel <emmanuelkibe148@gmail.com>
2025-09-17 12:23:17 +0300

    Created a viewset with CRUD permissions

--- budget_tracker/expenses/views.py
+++ budget_tracker/expenses/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Expense
+from .serializers import ExpenseSerializer
 
-# Create your views here.
+class ExpenseViewSet(viewsets.ModelViewSet):
+    serializer_class = ExpenseSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Expense.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
