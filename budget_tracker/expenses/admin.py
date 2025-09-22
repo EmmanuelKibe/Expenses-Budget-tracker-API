db7fb2b855c251ce27db6c1dcd7eb9c8f9cda2b9
Emmanuel <emmanuelkibe148@gmail.com>
2025-09-22 09:37:57 +0300

    Register the Category and Expense model

--- budget_tracker/expenses/admin.py
+++ budget_tracker/expenses/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from .models import Expense, Category
 
 # Register your models here.
 class ExpenseAdmin(admin.ModelAdmin):
@@ -7,4 +8,5 @@ class ExpenseAdmin(admin.ModelAdmin):
     list_filter = ("date", "budget")
     ordering = ("-date",)
 
-    
+admin.site.register(Expense, ExpenseAdmin)
+admin.site.register(Category)    
