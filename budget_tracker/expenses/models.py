a6907ff76693c6ccf5f7bc71df9b795606e929e9
Emmanuel <emmanuelkibe148@gmail.com>
2025-09-22 09:35:52 +0300

    Add Category model and link to Expense model

--- budget_tracker/expenses/models.py
+++ budget_tracker/expenses/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    user = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE, related_name="categories")
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
 class Expense(models.Model):
     user = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE, related_name='expenses')
     budget = models.ForeignKey('budget.Budget', on_delete=models.CASCADE, related_name='expenses')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name='expenses')
     name = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateField()
