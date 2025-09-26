952a0c4180cc350f0ed862faa10f7fd351e34c2f
Emmanuel <emmanuelkibe148@gmail.com>
2025-09-26 09:45:15 +0300

    Add CategoryViewSet to manage categories and link to authenticated users

--- budget_tracker/expenses/views.py
+++ budget_tracker/expenses/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets, permissions
-from .models import Expense
-from .serializers import ExpenseSerializer
+from .models import Expense, Category
+from .serializers import ExpenseSerializer, CategorySerializer
 
 class ExpenseViewSet(viewsets.ModelViewSet):
     serializer_class = ExpenseSerializer
@@ -12,3 +12,14 @@ class ExpenseViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
+class CategoryViewSet(viewsets.ModelViewSet):
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        # Only return categories belonging to the logged-in user
+        return Category.objects.filter(user=self.request.user).order_by('-id')
+
+    def perform_create(self, serializer):
+        # Auto-attach the user when creating a new category
+        serializer.save(user=self.request.user)
