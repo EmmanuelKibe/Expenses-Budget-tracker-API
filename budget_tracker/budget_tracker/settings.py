8f5bd5352789d80cfe2b269fad5c1a7fba13e3e4
Emmanuel <emmanuelkibe148@gmail.com>
2025-09-28 14:27:17 +0300

    Add STATIC_URL and STATIC_ROOT settings for static file management

--- budget_tracker/budget_tracker/settings.py
+++ budget_tracker/budget_tracker/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,3 +139,6 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
